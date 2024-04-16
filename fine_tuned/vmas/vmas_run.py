434dc5a6a72699fc5197200a24c7a8a5283b52cc
sudingli <sudingli@buaa.edu.cn>
2024-04-16 16:00:43 +0800

    add givewayto300, add tesla , 3090 config

--- fine_tuned/vmas/vmas_run.py
+++ fine_tuned/vmas/vmas_run.py
@@ -12,7 +12,7 @@ from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base=None, config_path="conf", config_name="config")
+@hydra.main(version_base=None, config_path="conf", config_name="config_give_way")
 def hydra_experiment(cfg: DictConfig) -> None:
     hydra_choices = HydraConfig.get().runtime.choices
     task_name = hydra_choices.task
